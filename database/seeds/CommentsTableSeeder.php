42464814c3360535bf51df6f04ffe9792dc67b0a
Mohammed Aidi <myster.oujda.48@gmail.com>
2022-11-16 18:00:19 +0100

    upsate seeder

--- database/seeds/CommentsTableSeeder.php
+++ database/seeds/CommentsTableSeeder.php
@@ -15,10 +15,23 @@ class CommentsTableSeeder extends Seeder
         $users = App\User::all();
 
         $numberComments = (int)$this->command->ask('How many Comments you want to create ?? ',200);
-        if($numberComments < 1)
+        if($numberComments < 1 && $posts->count == 0 && $users->count == 0)
         {
-            $this->command->info("You cannot create {$numberComments} comment by defalut 10 comments will be created");
-            $numberComments = 10;
+            if($numberComments < 1)
+            {
+                $this->command->info("You cannot create {$numberComments} comment \n By defalut 10 comments will be created");
+                $numberComments = 10;
+            }
+            elseif($posts->count ==0)
+            {
+                $this->command->info('To be able to create commnets, you must first create posts');
+                return;
+            }
+            elseif($users->count == 0)
+            {
+                $this->command->info('To be able eo create comments, you must first create accounts for users');
+                return;
+            }
         }
 
         factory(App\Comment::class,$numberComments)->make()->each(function($comment)use ($posts,$users){
